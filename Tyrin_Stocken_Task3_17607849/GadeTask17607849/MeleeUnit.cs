70ba70ecbc1fb6f379f058816d596483914884fd
TyrinStocken <43569563+TyrinStocken@users.noreply.github.com>
2018-09-25 11:43:59 +0200

    Add files via upload

---
+++ Tyrin_Stocken_Task3_17607849/GadeTask17607849/MeleeUnit.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace GadeTask17607849
+{
+    class MeleeUnit  : Unit
+    {
+        Map grid = new Map();
+
+        public MeleeUnit()
+        {
+            
+        }
+
+        public MeleeUnit(int xPosition, int yPosition, int health, int speed, int attack, int attackRange, string faction, string symbol, string name)
+        : base(xPosition, yPosition, health, speed, attack, attackRange, faction, symbol, name)
+        {
+        }
+        public override void combat(int v)
+        {
+        }
+        public override void moveUnit()
+        {
+        }
+        public override int closestUnit(int v)
+        {
+            double Distance, xSqr, ySqr;
+            int integerDistance, lowest;
+            int count = 0;
+            lowest = 400;
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    if(grid.map[i, j] != ".")
+                    {
+                        xSqr = Math.Pow((i - grid.mU[v].XPosition), 2);
+                        ySqr = Math.Pow((j - grid.mU[v].YPosition), 2);
+                        Distance = Math.Sqrt((xSqr + ySqr));
+                        integerDistance = Convert.ToInt32(Math.Round(Distance));
+                        if(integerDistance < lowest)
+                        {
+                            lowest = integerDistance;
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+        public override bool isAlive()
+        {
+            if (Health < 1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public override void save()
+        {
+            FileStream outFile = null;
+            StreamWriter writer = null;
+            try
+            {
+                outFile = new FileStream(@"Files\meleeUnit.txt", FileMode.Append, FileAccess.Write);
+                writer = new StreamWriter(outFile);
+
+                writer.WriteLine(xPosition);
+                writer.WriteLine(yPosition);
+                writer.WriteLine(health);
+                writer.WriteLine(speed);
+                writer.WriteLine(attack);
+                writer.WriteLine(attackRange);
+                writer.WriteLine(faction);
+                writer.WriteLine(symbol);
+                writer.WriteLine(name);
+            }
+            catch (Exception fe)
+            {
+                Debug.WriteLine(fe.Message);
+            }
+            finally
+            {
+                if (outFile != null)
+                {
+                    writer.Close();
+                    outFile.Close();
+                }
+            }
+        }
+        public override string ToString()
+        {
+            string output = Convert.ToString(XPosition);
+            output += Convert.ToString(YPosition);
+            output += Convert.ToString(Health);
+            output += Convert.ToString(Speed);
+            output += Convert.ToString(Attack);
+            output += Convert.ToString(AttackRange);
+            output += Faction;
+            output += Symbol;
+            output += name;
+
+            return output;
+        }
+    }
+}
---
+++ Tyrin_Stocken_Task3_17607849/GadeTask17607849/MoneyBuilding.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace GadeTask17607849
+{
+    class MoneyBuilding : Building
+    {
+        protected int xPosition;
+        protected int yPosition;
+        protected int health;
+        protected string faction;
+        protected string symbol;
+        public int money = 0;
+
+        public MoneyBuilding()
+        {
+            xPosition = 19;
+            yPosition = 0;
+            health = 100;
+            faction = "P";
+            symbol = "$";
+            money = 100;
+        }
+
+        public MoneyBuilding(int xPosition, int yPosition, int health, string faction, string symbol, int money)
+        : base(xPosition, yPosition, health, faction, symbol)
+        {
+            this.money = money;
+        }
+
+        override public bool isActive()
+        {
+            if (Health < 1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        override public string toString()
+        {
+            string output = Convert.ToString(xPosition);
+            output += Convert.ToString(yPosition);
+            output += Convert.ToString(health);
+            output += faction;
+            output += symbol;
+            return output;
+        }
+
+        public override void save()
+        {
+            FileStream outFile = null;
+            StreamWriter writer = null;
+            try
+            {
+                outFile = new FileStream(@"Files\moneyBuilding.txt", FileMode.Create, FileAccess.Write);
+                writer = new StreamWriter(outFile);
+
+                writer.WriteLine(xPosition);
+                writer.WriteLine(yPosition);
+                writer.WriteLine(health);
+                writer.WriteLine(faction);
+                writer.WriteLine(symbol);
+            }
+            catch (Exception fe)
+            {
+                Debug.WriteLine(fe.Message);
+            }
+            finally
+            {
+                if (outFile != null)
+                {
+                    writer.Close();
+                    outFile.Close();
+                }
+            }
+        }
+    }
+}
